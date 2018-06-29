0708e511c2eac320495ea5d846ff8322c64704a7
redstoone <cy_jing_2010@163.com>
2018-06-29 14:38:08 +0800

    修改一波

--- data/tpl_www/1_footer.php
+++ data/tpl_www/1_footer.php
@@ -3,6 +3,20 @@
 <script type="text/javascript">
 $(document).ready(function(){
 	$.cart.total();
+	var menuList = ['industry', 'match', 'local', 'interest', 'history', 'character', 'review', 'company']
+
+	var id = GetQueryString("id");
+	var cate = GetQueryString("cate");
+
+	if (id == 'news' && menuList.indexOf(cate) > 0) {
+		$('.menu ul li:eq('+ (menuList.indexOf(cate) + 1) +')').addClass('current');
+	}
+
+	function GetQueryString(name) {
+		var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+		var r = window.location.search.substr(1).match(reg);
+		if(r!=null)return  unescape(r[2]); return null;
+	}
 });
 </script>
 <?php echo $app->plugin_html_ap("phpokbody");?></body>
